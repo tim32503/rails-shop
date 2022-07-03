f2d793fa5784432ac6e517d45ba96f761faaad4f
Tim Hsueh <timhsueh@curihaosity.xyz>
2022-07-03 15:37:30 +0800

    fix: 修改 User 欄位名稱

--- db/migrate/20220703061558_create_users.rb
+++ db/migrate/20220703061558_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :email
       t.string :encrypted_password
       t.string :name
       t.string :auth_token
