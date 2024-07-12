4535f4df3dc2144a717782b4582e9c7af7e0a6e1
Tim Hsueh <timhsueh@curihaosity.xyz>
2024-07-12 14:29:21 +0800

    fix: 修正為正確引入 faker 導致部署失敗之異常

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 # 使用 Faker 產生產品假資料
 50.times do
   Product.create(
